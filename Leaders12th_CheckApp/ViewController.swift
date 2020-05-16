78374a17603f06256f920f6f001518993a21e65a
obaya884 <taku3.884@gmail.com>
2020-05-16 21:53:56 +0900

    Initial Commit

---
+++ Leaders12th_CheckApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Leaders12th_CheckApp
+//
+//  Created by 大林拓実 on 2020/05/16.
+//  Copyright © 2020 Obayashi Takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
